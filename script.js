5b59ec6114baf4bbd7bc6d0b8dc54bbd9ebb564b
Fayhild <gabriel.schaal27@gmail.com>
2023-04-27 19:46:09 +0200

    big commit, added all essential content

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function menuOnClick() {
     document.getElementById("nav-liens").classList.toggle("menu-mobile");
    
   }
+
+  window.onload = function() {
+    setTimeout(function() {
+        document.querySelector('.fade-in-img').classList.add('show');
+    }, 200);
+}
+
+/* permet de faire apparaître la navabr lors du scroll vers le haut
+*/
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nava").style.top = "0";
+  } else {
+    document.getElementById("nava").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
