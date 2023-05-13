653cfe27a5eb8f3accb720157d88b6f02db02e0c
Fayhild <gabriel.schaal27@gmail.com>
2023-05-13 18:08:17 +0200

    First modal window ever done ! project section WIP

--- script.js
+++ script.js
@@ -28,3 +28,70 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 } 
+
+/* ______________________________________________________________________________________*/
+/* FENETRES MODALES */ 
+
+/* FENETRE MODALE STI2D */
+
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+function toggleModal(){
+  modalContainer.classList.toggle("active")
+}
+
+/* */
+
+const items = document.querySelectorAll('.containerSlider img');
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.leftBtn');
+let count = 0;
+
+function slideSuivante(){
+    items[count].classList.remove('active');
+
+    if(count < nbSlide - 1){
+        count++;
+    } else {
+        count = 0;
+    }
+
+    items[count].classList.add('active')
+    console.log(count);
+    
+}
+suivant.addEventListener('click', slideSuivante)
+
+
+function slidePrecedente(){
+    items[count].classList.remove('active');
+
+    if(count > 0){
+        count--;
+    } else {
+        count = nbSlide - 1;
+    }
+
+    items[count].classList.add('active')
+    // console.log(count);
+    
+}
+precedent.addEventListener('click', slidePrecedente)
+
+function keyPress(e){
+    console.log(e);
+    
+    if(e.keyCode === 37){
+        slidePrecedente();
+    } else if(e.keyCode === 39){
+        slideSuivante();
+    }
+}
+document.addEventListener('keydown', keyPress)
+
+/* FENETRE MODALE STI2D */
+
